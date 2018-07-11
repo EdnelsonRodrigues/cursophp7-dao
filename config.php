c6c44a5886868eed837040b26bacf417664940e8
Ednelson Rodrigues <edrodrigues.inf@gmail.com>
2018-07-11 20:35:20 -0300

    iniciamos o projeto

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+
+spl_autoload_register(function($nameClass){
+
+	$filename = "class" . DIRECTORY_SEPARATOR . $nameClass . ".php";
+
+	if (file_exists(($filename))) {
+
+		require_once($filename);
+
+	}
+
+});
+
+?>
